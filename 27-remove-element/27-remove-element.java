1be961a54f709d5be823623b68e907b93e421017
yiru <43528898+yiruW@users.noreply.github.com>
2022-05-19 17:07:03 -0400

    Time: 0 ms (100.00%), Space: 43 MB (11.67%) - LeetHub

---
+++ 27-remove-element/27-remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0, fast = 0;
+        while(fast < nums.length){
+            if(nums[fast] == val){
+                fast ++;
+            }else{
+                nums[slow] = nums[fast];
+                slow ++;
+                fast ++;
+            }
+        }
+        return slow;
+    }
+}
