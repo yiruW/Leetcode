8241db5a673bfb7f2ccd79222457131c1fef0228
yiru <43528898+yiruW@users.noreply.github.com>
2022-05-20 16:41:15 -0400

    Time: 2 ms (29.72%), Space: 58.2 MB (12.64%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length-1;
+        while(left < right){
+            s[left] ^= s[right];
+            s[right] ^= s[left];
+            s[left] ^= s[right];
+            left ++;
+            right --;
+        }
+    }
+}
