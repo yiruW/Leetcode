da61f8102c00814f6f9c9828bdd8bc17ac128ed6
yiru <43528898+yiruW@users.noreply.github.com>
2022-05-24 15:06:49 -0400

    Time: 134 ms (8.27%), Space: 41.5 MB (99.46%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,20 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    var stack = [];
-    if(s.length%2 != 0){return false;}
-    for(var i=0; i<s.length; i++){
-        const c = s.charAt(i);
-        console.log(stack, c);
-        if(c === '(' || c === '{' || c === '['){
-            stack.push(c);
-        }else{
-            const outC = stack.pop();
-            if((c === ')' && outC != '(') ||(c === ']' && outC != '[') || (c === '}' && outC != '{')){
-                return false;
-            }
+    const stack = [];
+    
+    for (let i = 0 ; i < s.length ; i++) {
+        let c = s.charAt(i);
+        switch(c) {
+            case '(': stack.push(')');
+                break;
+            case '[': stack.push(']');
+                break;
+            case '{': stack.push('}');
+                break;
+            default:
+                if (c !== stack.pop()) {
+                    return false;
+                }
         }
     }
-    if(stack.length > 0){return false;}
-    return true;
+    
+    return stack.length === 0;
 };
