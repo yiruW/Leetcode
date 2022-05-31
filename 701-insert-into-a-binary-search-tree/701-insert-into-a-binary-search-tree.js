a74184ca1906be97ffa655ec8c56acf155bfe735
yiru <43528898+yiruW@users.noreply.github.com>
2022-05-30 23:20:03 -0400

    Time: 108 ms (96.25%), Space: 49.6 MB (91.92%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.js
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.js
@@ -12,25 +12,17 @@
  * @return {TreeNode}
  */
 var insertIntoBST = function(root, val) {
-    if(root == null) return new TreeNode(val);
-    let parent = new TreeNode(0);
     const findPos = (node, tar) =>{
         if(node === null){
             let newNode = new TreeNode(tar);
-            if(parent.val < tar){
-                parent.right = newNode;
-            }else{
-                parent.left = newNode;
-            }
-            return;
+            return newNode;
         }
-        parent = node;
         if(node.val > tar){
-            findPos(node.left, tar);
-        }else{
-            findPos(node.right, tar);
+            node.left = findPos(node.left, tar);
+        }else if(node.val < tar){
+            node.right = findPos(node.right, tar);
         }
+        return node;
     }
-    findPos(root, val);
-    return root;
+    return findPos(root, val);
 };
