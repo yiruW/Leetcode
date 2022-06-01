6ad89d1581150f4617bd30e470e1efcfbcfe29f6
yiru <43528898+yiruW@users.noreply.github.com>
2022-05-31 22:36:34 -0400

    Time: 110 ms (9.00%), Space: 42 MB (52.71%) - LeetHub

---
+++ 198-house-robber/198-house-robber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    let len = nums.length;
+    if(len == 1){return nums[0];}
+    let dp = [nums[0], Math.max(nums[0], nums[1])];
+    for(let i=2; i<len; i++){
+        dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+    }
+    return Math.max(dp[len-1], dp[len-2]);
+};
