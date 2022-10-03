1aade6cf2a4ec45f0a210cd4c64bd59dc5f96140
yiru wu <43528898+yiruW@users.noreply.github.com>
2022-10-03 00:15:56 -0400

    Time: 65 ms (75.43%), Space: 15.4 MB (48.86%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        nodeList = []
+        def levelDetect(node: Node, depth: int):
+            if not node: return []
+            if len(nodeList) == depth: 
+                nodeList.append([])
+                node.next = None
+                nodeList[depth].append(node)
+            else:
+                node.next = nodeList[depth][len(nodeList[depth])-1]
+                nodeList[depth].append(node)
+            if node.right: levelDetect(node.right, depth+1)
+            if node.left: levelDetect(node.left, depth+1)
+        levelDetect(root, 0)
+        return root
